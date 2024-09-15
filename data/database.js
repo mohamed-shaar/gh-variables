0516973b1ce155b4c60c3cf95bae6ffc312e1cd3
Mohamed ElShaar <mamo.sha3ar@gmail.com>
2024-09-15 16:49:15 +0300

    fixed setup and added secrets

--- data/database.js
+++ data/database.js
@@ -1,12 +1,18 @@
-import { MongoClient } from 'mongodb';
+import { MongoClient, ServerApiVersion } from 'mongodb';
 
 const clusterAddress = process.env.MONGODB_CLUSTER_ADDRESS;
 const dbUser = process.env.MONGODB_USERNAME;
 const dbPassword = process.env.MONGODB_PASSWORD;
 const dbName = process.env.MONGODB_DB_NAME;
 
-const uri = `mongodb+srv://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri);
+const uri = `mongodb+srv://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority&appName=Cluster0`;
+const client = new MongoClient(uri,  {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
 
 console.log('Trying to connect to db');
 
@@ -15,6 +21,7 @@ try {
   await client.db(dbName).command({ ping: 1 });
   console.log('Connected successfully to server');
 } catch (error) {
+  console.log(error);
   console.log('Connection failed.');
   await client.close();
   console.log('Connection closed.');
